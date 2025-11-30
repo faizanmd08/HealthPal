649c1ba20c0b2ad097b54bfaf0ae4ffcb8e4c2a8
faizanmd08 <mdfaizan08iitp@gmail.com>
2025-11-30 18:12:15 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Mohammad Faizan",
+    author_email="mdfaizan08iitp@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
